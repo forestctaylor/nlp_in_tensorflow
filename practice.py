d5485c2fcf0aeef10edac08dcbf2faf7d2fc7904
Forest Cummings-Taylor <forestctaylor@gmail.com>
2020-08-04 18:54:05 -0500

    Add OOV token for out of vocabulary words

--- practice.py
+++ practice.py
@@ -8,11 +8,14 @@ sentences = [
 ]
 
 if __name__ == '__main__':
-    tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=100) # num_words hyperparameter set to much higher value than necessary
+    # num_words hyperparameter set to much higher value than necessary
+    # oov_token used in sequence when word does not appear in dictionary
+    tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=100, oov_token="<OOV>") 
     tokenizer.fit_on_texts(sentences)
     word_index = tokenizer.word_index 
 
-    sequences = tokenizer.texts_to_sequences(sentences)
+    sequences = tokenizer.texts_to_sequences(['Some other damn sentence', *sentences])
 
     padded_sequences = tf.keras.preprocessing.sequence.pad_sequences(sequences, padding="post")
-    
+    print("Dictionary: {}".format(word_index))
+    print("Padded seq: {}".format(padded_sequences))
