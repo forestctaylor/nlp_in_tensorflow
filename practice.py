cb68bc79f31d348566a6b630706cc8cba4195f7d
Forest Cummings-Taylor <forestctaylor@gmail.com>
2020-08-04 18:50:58 -0500

    Add pad_sequences() method

--- practice.py
+++ practice.py
@@ -1,12 +1,18 @@
 import tensorflow as tf
-from tensorflow.keras.preprocessing.text import Tokenizer 
 
 sentences = [
     'I love my dog',
-    'I love my cat'
+    'I love my cat',
+    'You love my dog!',
+    'Do you think my dog is amazing?'
 ]
 
 if __name__ == '__main__':
-    tokenizer = Tokenizer(num_words=100) # num_words hyperparameter set to much higher value than necessary
+    tokenizer = tf.keras.preprocessing.text.Tokenizer(num_words=100) # num_words hyperparameter set to much higher value than necessary
     tokenizer.fit_on_texts(sentences)
-    print(tokenizer.word_index)
+    word_index = tokenizer.word_index 
+
+    sequences = tokenizer.texts_to_sequences(sentences)
+
+    padded_sequences = tf.keras.preprocessing.sequence.pad_sequences(sequences, padding="post")
+    
